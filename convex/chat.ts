939e5ff5840dfe2dd072978f076c247407204f7d
skiupace <ahmedalhakemee200335@gmail.com>
2025-08-04 02:56:15 +0300

    initial commit

---
+++ convex/chat.ts
@@ -0,0 +1,53 @@
+import { query, mutation, internalAction } from "./_generated/server";
+import { api, internal } from "./_generated/api";
+import { v } from "convex/values";
+
+export const sendMessage = mutation({
+  args: {
+    user: v.string(),
+    body: v.string(),
+  },
+  handler: async (ctx, args) => {
+    console.log("This TypeScript function is running on the server.");
+    await ctx.db.insert("messages", {
+      user: args.user,
+      body: args.body,
+    });
+
+    if (args.body.startsWith("/wiki")) {
+      const topic = args.body.slice(args.body.indexOf(" ") + 1);
+      await ctx.scheduler.runAfter(0, internal.chat.getWikipediaSummary, {
+        topic,
+      });
+    }
+  },
+});
+
+export const getMessages = query({
+  args: {},
+  handler: async (ctx) => {
+    const messages = await ctx.db.query("messages").order("desc").take(50);
+    return messages.reverse();
+  },
+});
+
+export const getWikipediaSummary = internalAction({
+  args: { topic: v.string() },
+  handler: async (ctx, args) => {
+    const response = await fetch(
+      "https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&titles=" +
+        args.topic
+    );
+
+    const summary = getSummaryFromJSON(await response.json());
+    await ctx.scheduler.runAfter(0, api.chat.sendMessage, {
+      user: "Wikipedia",
+      body: summary,
+    });
+  },
+});
+
+function getSummaryFromJSON(data: any) {
+  const firstPageId = Object.keys(data.query.pages)[0];
+  return data.query.pages[firstPageId].extract;
+}
---
+++ src/App.tsx
@@ -0,0 +1,75 @@
+import { useEffect, useState } from "react";
+import { faker } from "@faker-js/faker";
+
+import { useQuery, useMutation } from "convex/react";
+import { api } from "../convex/_generated/api";
+
+// For demo purposes. In a real app, you'd have real user data.
+const NAME = getOrSetFakeName();
+
+export default function App() {
+  const messages = useQuery(api.chat.getMessages);
+
+  const sendMessage = useMutation(api.chat.sendMessage);
+
+  const [newMessageText, setNewMessageText] = useState("");
+
+  useEffect(() => {
+    // Make sure scrollTo works on button click in Chrome
+    setTimeout(() => {
+      window.scrollTo({ top: document.body.scrollHeight, behavior: "smooth" });
+    }, 0);
+  }, [messages]);
+
+  return (
+    <main className="chat">
+      <header>
+        <h1>Convex Chat</h1>
+        <p>
+          Connected as <strong>{NAME}</strong>
+        </p>
+      </header>
+      {messages?.map((message) => (
+        <article
+          key={message._id}
+          className={message.user === NAME ? "message-mine" : ""}
+        >
+          <div>{message.user}</div>
+
+          <p>{message.body}</p>
+        </article>
+      ))}
+      <form
+        onSubmit={async (e) => {
+          e.preventDefault();
+          await sendMessage({ user: NAME, body: newMessageText });
+          setNewMessageText("");
+        }}
+      >
+        <input
+          value={newMessageText}
+          onChange={async (e) => {
+            const text = e.target.value;
+            setNewMessageText(text);
+          }}
+          placeholder="Write a message…"
+          autoFocus
+        />
+        <button type="submit" disabled={!newMessageText}>
+          Send
+        </button>
+      </form>
+    </main>
+  );
+}
+
+function getOrSetFakeName() {
+  const NAME_KEY = "tutorial_name";
+  const name = sessionStorage.getItem(NAME_KEY);
+  if (!name) {
+    const newName = faker.person.firstName();
+    sessionStorage.setItem(NAME_KEY, newName);
+    return newName;
+  }
+  return name;
+}
